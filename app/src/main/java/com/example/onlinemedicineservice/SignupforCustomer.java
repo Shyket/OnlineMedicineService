6331502fd65367428559490c70396c910f5f9030
Shyket <munimulislam@iut-dhaka.edu>
2020-03-10 18:33:24 +0600

    2nd push

--- app/src/main/java/com/example/onlinemedicineservice/SignupforCustomer.java
+++ app/src/main/java/com/example/onlinemedicineservice/SignupforCustomer.java
@@ -10,5 +10,6 @@ public class SignupforCustomer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signupfor_customer);
+
     }
 }
